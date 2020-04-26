06b1cc65efb2c5e0e510a46b1469c4d10b90e222
Farkhod <sadykovfarkhod@gmail.com>
2020-04-26 04:07:08 -0500

    Configured MYSQL and ready to deploy to dev

--- deployments/docker/academy/settings.py
+++ deployments/docker/academy/settings.py
@@ -92,10 +92,21 @@ WSGI_APPLICATION = 'wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get('MYSQL_USER'),
+        'USER': os.environ.get('MYSQL_DATABASE'),
+        'PASSWORD': os.environ.get('MYSQL_PASSWORD'),
+        'HOST': os.environ.get('MYSQL_HOST'),
+        'PORT': '3306',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 
 # Password validation
