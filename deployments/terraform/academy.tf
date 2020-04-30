a365564e2cd279b308c5a546d4f4f8914605719a
bkarakas <bkarakasu@gmail.com>
2020-04-29 20:50:22 -0500

    removed added template_custom_vars from academy.tf

--- deployments/terraform/academy.tf
+++ deployments/terraform/academy.tf
@@ -6,19 +6,9 @@ module "academy-deploy" {
   deployment_endpoint    = "${lookup(var.deployment_endpoint, "${var.deployment_environment}")}"
   deployment_path        = "academy"
 
-  template_custom_vars  = {
-    mysql_password       = "${var.mysql_password}"
-    mysql_root_password  = "${var.mysql_root_password}"
-    mysql_user           = "${var.mysql_user}"
-    mysql_database       = "${var.mysql_database}"
-    deployment_image     = "${var.deployment_image}"
-    admin_user           = "${var.admin_user}"
-    admin_password       = "${var.admin_password}"
-    service_account      = "${var.academy_service_account}"
-    github_token         = "${var.github_token}"
-    github_client_id     = "${lookup(var.github_client_id, "${var.deployment_environment}")}"
-    github_client_secret = "${lookup(var.github_client_secret, "${var.deployment_environment}")}"
-    application_secret   = "${var.application_secret}"
+  template_custom_vars  = {     
+    deployment_image     = "${var.deployment_image}"    
+    service_account      = "${var.academy_service_account}"    
     academy_credentials  = "${kubernetes_secret.academy_credentials.metadata.0.name}"
   }
 }
