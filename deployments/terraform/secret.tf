9c3476b01b389b26616b80c6e3420a0889ece43b
bkarakas <bkarakasu@gmail.com>
2020-04-29 20:29:53 -0500

    keys are placed to the right place

--- deployments/terraform/secret.tf
+++ deployments/terraform/secret.tf
@@ -5,35 +5,35 @@ resource "kubernetes_secret" "academy_credentials" {
    }
 
    data {
-       name = "mysql_user"
-       value = "${var.mysql_user}"
+       
+       mysql_user = "${var.mysql_user}"
 
-       name = "mysql_database"
-       value = "${var.mysql_database}"
+       
+       mysql_database = "${var.mysql_database}"
 
-       name = "mysql_password"
-       value = "${var.mysql_password}"
+      
+       mysql_password = "${var.mysql_password}"
 
-       name = "admin_user"
-       value = "${var.admin_user}"
+       
+       admin_user = "${var.admin_user}"
 
-       name = "admin_password"
-       value = "${var.admin_password}"
+       
+       admin_password = "${var.admin_password}"
 
-       name = "mysql_root_password"
-       value = "${var.mysql_root_password}"
+       
+       mysql_root_password = "${var.mysql_root_password}"
 
-       name = "github_token"
-       value = "${var.github_token}"
+       
+       github_token = "${var.github_token}"
 
-       name = "github_client_id"
-       value = "${var.github_client_id}"
+       
+       github_client_id = "${var.github_client_id}"
 
-       name = "github_client_secret"
-       value = "${var.github_client_secret}"
+       
+       github_client_secret = "${var.github_client_secret}"
 
-       name = "application_secret"
-       value = "${var.application_secret}"
+       
+       application_secret = "${var.application_secret}"
          
    }
 }
